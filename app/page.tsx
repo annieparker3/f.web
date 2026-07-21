1386dd5b27757fa1dc31da87f6afb0634dab66f6
kwekuadjei44-design <kwekuadjei44@gmail.com>
2026-07-21 14:44:21 +0000

    Fix build: remove unused AnimatedLogo import

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import AnimatedLogo from "@/components/AnimatedLogo";
 import {
   Settings,
   ArrowRight,
